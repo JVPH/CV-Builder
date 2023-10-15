bac4c482751e2b41623be56f1257d3088ea01594
JVPH <gyrospizzeria@protonmail.com>
2023-10-15 12:38:21 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <PersonalDetails />
+    </>
+  );
+}
+
+function PersonalDetails() {
+  return (
+    <>
+      <h1>Personal Details</h1>
+    </>
+  );
+}
+
+export default App;
